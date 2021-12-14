61723b6a37b10b2f5d0ced11f8a6acc3e83bb7ce
adnan bouthir <adnanbouthir@gmail.com>
2021-12-14 11:25:00 +0100

    Complete addTodo function

--- app.js
+++ app.js
@@ -10,4 +10,27 @@ todoButton.addEventListener('click', addTodo);
 function addTodo(event){
     // prevent default behavior form submit
      event.preventDefault();
+    //  todo Div
+    const todoDiv = document.createElement('div');
+    todoDiv.classList.add('todo');
+    // create LI
+    const newTodo = document.createElement('li');
+    newTodo.innerText = "hey";
+    newTodo.classList.add('todo-item');
+    // append li as child of div
+    todoDiv.appendChild(newTodo);
+
+    // check mark button
+    const completed = document.createElement('button');
+    completed.innerHTML = '<i class ="fas fa-check"></i>';
+    completed.classList.add('complete-btn');
+    todoDiv.appendChild(completed);
+     // delete mark button
+     const trash = document.createElement('button');
+     trash.innerHTML = '<i class ="fas fa-trash"></i>';
+     trash.classList.add('complete-btn');
+     todoDiv.appendChild(trash);
+    //  append to list
+    todoList.appendChild(todoDiv);
+ 
 }
